2533e02c19f643a9e343e1dd5f96252da672f346
teramotodaiki <i101323@gmail.com>
2023-01-01 21:53:44 +0900

    可視化

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,83 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+// @ts-expect-error tsnejs is not typed
+import { tSNE } from "@jwalsh/tsnejs/lib";
+import { Chart } from "chart.js/auto";
+import { parse } from "csv-parse";
+import fs from "fs/promises";
+import Head from "next/head";
+import { useEffect, useRef } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+export async function getStaticProps(context) {
+  // const csv = await fs.readFile("./public/slim.csv", "utf8");
+  const csv = await fs.readFile("./public/helpdata_help_embedding.csv", "utf8");
+  const records = await new Promise((resolve, reject) => {
+    parse(csv, { trim: true, columns: true }, (err, records) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(records);
+      }
+    });
+  });
 
-export default function Home() {
+  const limit = 1525; // これ以上大きくするとなぜか結果がnullになる
+
+  const opt = {
+    epsilon: 10, // epsilon is learning rate (10 = default)
+    perplexity: 30, // roughly how many neighbors each point influences (30 = default)
+    dim: 2, // dimensionality of the embedding (2 = default)
+  };
+
+  const tsne = new tSNE(opt); // create a tSNE instance
+
+  // initialize data. Here we have 3 points and some example pairwise dissimilarities
+  const dists = records
+    .slice(0, limit)
+    .map((record) => JSON.parse(record["ada_embedding"]));
+  tsne.initDataDist(dists);
+
+  // every time you call this, solution gets better
+  [...Array(500)].forEach((_, i) => tsne.step());
+  const vectors = tsne.getSolution(); // Y is an array of 2-D points that you can plot
+
+  return {
+    props: {
+      vectors,
+    },
+  };
+}
+
+export default function Home(props) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const chartRef = useRef<Chart>();
+  useEffect(() => {
+    if (!canvasRef.current || chartRef.current) {
+      return;
+    }
+
+    console.log("vectors", props.vectors);
+
+    const chart = new Chart(canvasRef.current, {
+      type: "scatter",
+      data: {
+        // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "散布図",
+            data: props.vectors.map((arr) => ({ x: arr[0], y: arr[1] })),
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+    chartRef.current = chart;
+  });
+
   return (
     <>
       <Head>
@@ -14,110 +86,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <h1>t-SNE</h1>
+        <canvas ref={canvasRef}></canvas>
       </main>
     </>
-  )
+  );
 }
